12ee9a61c30fcfa257ee661c57a7174cc1884b08
Toshi <yttoshi0919@gmail.com>
2019-12-12 23:53:23 +0900

    GlaobalFooterを作った。 #3

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,15 +3,18 @@
     <GlobalHeader />
     <nuxt />
     <p>フッターだよ</p>
+    <GlobalFooter />
   </div>
 </template>
 
 <script>
 import GlobalHeader from "~/components/organisms/GlobalHeader.vue";
+import GlobalFooter from "~/components/organisms/GlobalFooter.vue";
 
 export default {
   components: {
     GlobalHeader,
+    GlobalFooter,
   }
 }
 </script>
