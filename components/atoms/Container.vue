7ff78033b7b3af2c9a055b74352cf95f0c267f01
Toshi <yttoshi0919@gmail.com>
2019-12-18 23:31:32 +0900

    Container.vueと_variable.scssを作成した。 #3

---
+++ components/atoms/Container.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container" :class="{'container--fluid': fluid}">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/atoms/LinkButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <component
+    :is="componentIs"
+    :to="to"
+    :href="href"
+    class="link-button"
+    tabindex="0"
+  >
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+    },
+    href: {
+      type: String,
+    },
+    componentIs: {
+      type: String,
+      default: 'nuxt-link',
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.link-button{
+  display: inline-block;
+}
+</style>
