20f25c919ef6f222ce15752fcf10936a47af71c2
Toshi <yttoshi0919@gmail.com>
2020-02-05 14:08:51 +0900

    nuxt.configに付けたし #5

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,8 +12,15 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      //{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
+  },
+  router: {
+    base: '/2020/',
+    trailingSlash: true,
+  },
+  generate: {
+    routes: ['404']
   },
   /*
   ** Customize the progress-bar color
