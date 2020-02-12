38478f0da8d5f26c0bbc954ef11a9ed35a7e0090
toshi-bp <53927502+toshi-bp@users.noreply.github.com>
2020-02-12 16:10:47 +0900

    footerのスマホ版を作成縦長にした。あと、レイアウトをアレンジした。 #16 (#17)
    確認よろしくお願いします

--- components/organisms/GlobalFooter.vue
+++ components/organisms/GlobalFooter.vue
@@ -3,16 +3,17 @@
     <footer class="footer">
     <div>
       <img src="~/assets/image/symbol.svg" class="footer__logo">
-      <p class="footer__welcome">入れ。</p>
     </div>
     <div class="footer__copyright">
       <p>2020年度
-        
+
         東京理科大学野田地区
         <br>新入生歓迎ガイダンス実行委員会</p>
     </div>
+    <p class="footer__welcome">一緒に理大祭を作りませんか？</p>
     <div class="footer__list">
       <ul>
+        <li class="footer__list__item"><fa icon="desktop" fixed-width/><a href="" target="blank">理大祭新歓ウェブサイト</a></li>
         <li class="footer__list__item"><fa :icon="['fab', 'twitter']" fixed-width/><a href="https://twitter.com/noda_ridaisai" target="blank">理大祭公式Twitter</a></li>
         <li class="footer__list__item"><fa icon="envelope" fixed-width/><a href="mailto:festival@ed.tus.ac.jp" @click="$ga.event('link', 'click', 'email', 1)">festival@ed.tus.ac.jp</a></li>
       </ul>
@@ -24,27 +25,36 @@
 <style lang="scss" scoped>
 .footer {
   position:relative;
-  height: 12rem;
+  height: 14rem;
   background: $color;
   // bottom:0 今後残すか考える;
   z-index: $z-index-global-footer;
-  @include media-breakpoint-down(sm){
-    height: 40rem;
-  }
+
    @include media-breakpoint-down(md){
     height: 9.5rem;
+  }
+
+  @include media-breakpoint-down(sm){
+    height: 20rem;
+    display: block;
   }
 
   &__logo{
     width: 7rem;
     margin: 2rem 0 1rem 2rem ;
-    @include media-breakpoint-down(sm){
-     width: 5rem;
-     margin: 2rem 0 1rem 1.5rem ;
-     }
+
    @include media-breakpoint-down(md){
      width: 5rem;
      margin: 1.5rem 0 0.8rem 1.5rem ;
+     }
+
+     @include media-breakpoint-down(sm){
+     width: 5rem;
+     display: block;
+     margin: 0rem auto 1rem auto ;
+     padding-top: 1rem;
+     //position: absolute;
+     //top: 1rem;
      }
   }
   &__copyright{
@@ -52,17 +62,24 @@
     color:#fff;
     font-family:$theme-font;
     font-size:15px;
-    display:inline-block;
+    display:block;
     top:2.3rem;
     left:11em;
-      @include media-breakpoint-down(sm){
-        font-size:10px;
-      }
+
        @include media-breakpoint-down(md){
         padding-right:28%;
         font-size:11px;
         top:1.6rem;
         left:7.5rem;
+      }
+
+      @include media-breakpoint-down(sm){
+        padding-right: 0;
+        display: block;
+        font-size:10px;
+        text-align: center;
+        position: static;
+        //left: 2rem;
       }
   }
   &__welcome{
@@ -71,21 +88,29 @@
     font-family:$theme-font;
     font-size:30px;
     display:inline-block;
-    top:4rem;
-    left:30rem;
-      @include media-breakpoint-down(sm){
-        font-size:20px;
-      }
+    top:2rem;
+    left:35rem;
+
        @include media-breakpoint-down(md){
         font-size:20px;
-        top:3rem;
-        left:20rem;
+        top:1.4rem;
+        left:23rem;
+      }
+
+      @include media-breakpoint-down(sm){
+        font-size:18px;
+        display: block;
+        position: static;
+        text-align: center;
+        //top: 11rem;
+        //left: 25%;
+        margin-top: 0.5rem;
       }
  }
   &__list{
   position:absolute;
-  top:6rem;
-  left:8rem;
+  top:5.5rem;
+  left:32.5rem;
   //margin:0 2rem 0 0;
   display:inline-block;
 
@@ -97,27 +122,12 @@
     }
   }
 
-  @include media-breakpoint-down(sm){
-   &__list{
-    position:absolute;
-    top:4rem;
-    right: 0rem;
-    //margin:0 2rem 0 0;
-    display:inline-block;
-
-      &__item{
-      list-style:none;
-      color:#fff;
-      margin-bottom:1rem;
-        }
-     }
-  }
 
    @include media-breakpoint-down(md){
    &__list{
     position:absolute;
     top:4rem;
-    left: 5rem;
+    left: 20.5rem;
     //margin:0 2rem 0 0;
     display:inline-block;
 
@@ -126,6 +136,27 @@
       color:#fff;
       margin-bottom:0.3rem;
       font-size:14px;
+        }
+     }
+  }
+
+  @include media-breakpoint-down(sm){
+   &__list{
+    //position:absolute;
+    //top:14rem;
+    //left: 25%;
+    margin:0 auto 0 auto;
+    padding-right: 2rem;
+    position: static;
+    display:block;
+    text-align: center;
+
+      &__item{
+      list-style:none;
+      color:#fff;
+      display:block;
+      text-align: center;
+      //margin-bottom:1rem;
         }
      }
   }
