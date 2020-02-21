44f95be176ae96863438b6c3f22aa276c9e99112
Toshi <yttoshi0919@gmail.com>
2020-02-21 11:03:13 +0900

    ボタンと、comingsoonのページ的なものを作った。 いらなかったら消してしまえ。 #21

--- components/atoms/LinkButton.vue
+++ components/atoms/LinkButton.vue
@@ -32,6 +32,30 @@ export default {
   display: inline-block;
   padding: 0.8rem 2.5rem;
   border: 2px solid $theme-color;
-  color: $them-color;
+  color: $theme-color;
+  font-weight: bold;
+  text-align: center;
+  transition: 0.15s ease all;
+  line-height: 1;
+  background: #ffffff;
+
+  &:hover {
+    background: $theme-color;
+    color: #ffffff;
+    text-decoration: none;
+  }
+
+  &:active {
+    $darken: darken($color: $theme-color, $amount: 5);
+    //上で設定した色を定める。明度、％
+    background: $darken;
+    border-color: $darken;
+    color:  #ffffff;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 3px rgba($color: $theme-color, $alpha: 0.3);
+  }
 }
 </style>
