b5c1974513f84df6f31424b57e63d1d430096403
Toshi <yttoshi0919@gmail.com>
2020-03-08 15:11:47 +0900

    jsonの作成 #38
    Co-authored-by: Soji / SofPyon <sofpyon@hrgrweb.com>

---
+++ circles/convertToJson.js
@@ -0,0 +1,65 @@
+if (typeof window === 'undefined') {
+  const fs = require("fs");
+
+  const CSV_FILE_PATH = 'circleList.csv';
+  const JSON_FILE_PATH = 'circleList.json';
+
+  const TITLES = {};
+  TITLES['団体ID'] = 'circle_id';
+  TITLES['団体名'] = 'name';
+  TITLES['仕分け'] = 'type';
+  TITLES['教室ID'] = 'booth';
+  TITLES['活動日'] = 'days';
+  TITLES['活動場所'] = 'place';
+  TITLES['所属人数'] = 'people_all';
+  TITLES['男子所属人数'] = 'people_male';
+  TITLES['女子所属人数'] = 'people_female';
+  TITLES['代表者'] = 'leader';
+  TITLES['部費'] = 'club_dues';
+  TITLES['入部条件'] = 'qualifications';
+  TITLES['サークル概要'] = 'subtitle';
+  TITLES['活動内容'] = 'summar';
+  TITLES['@イメージ画像'] = 'image_filename';
+  TITLES['TwitterアカウントID'] = 'twitter_ids';
+  TITLES['ホームページのリンク(任意)'] = 'url';
+  TITLES['新入生連絡先メールアドレス(任意)'] = 'email';
+  TITLES['InstagramアカウントID(任意)'] = 'instagram_ids';
+
+  const raw_data = fs.readFileSync(CSV_FILE_PATH, 'utf-8');
+  const array_data = raw_data.split("\n");
+
+  // タイトル行
+  const titles = array_data[0].split(",");
+
+  // 返却するデータ
+  let return_data = [];
+
+  // 0 行目はタイトル行なので、i = 0 ではなく i = 1 からループを始める
+  for (let i = 1; i < array_data.length; ++i) {
+    const line = array_data[i].split(",");
+    for (let j = 0; j < line.length; ++j) {
+      if (typeof return_data[i - 1] !== 'object') {
+        return_data[i - 1] = {};
+      }
+
+      if (!(titles[j] in TITLES)) {
+        continue;
+      }
+
+      let item = line[j];
+
+      if (titles[j] === '仕分け') {
+        item = {
+          u: 'physical',
+          o: 'musical',
+          b: 'cultual',
+        }[item];
+      }
+
+      return_data[i - 1][TITLES[titles[j]]] = item;
+    }
+  }
+
+  return_json = JSON.stringify(return_data);
+  fs.writeFileSync(JSON_FILE_PATH, return_json + "\n");
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,43 @@
-//自動化する作業を入れる
-"use strict"
-const fs = require("fs")
-const gulp = require("gulp")
-const ejs = require("gulp-ejs")
-const rename = require("gulp-rename")
-const clean = require("gulp-clean")
+'use strict';
+const fs = require('fs');
+const gulp = require('gulp');
+const ejs = require('gulp-ejs');
+const rename = require('gulp-rename');
+const clean = require('gulp-clean');
+
+
+gulp.task('ejs:booths', function (done) {
+  const jsonFile = './circles/circlesList.json';
+  const templateFile = './booths/ejs/_booth.vue.ejs';
+  const indexFile = './booths/ejs/index.vue.ejs';
+  const dest = './pages/booths/';
+  const json = JSON.parse(fs.readFileSync(jsonFile, 'utf8'));
+  const list = json.list;
+  let id;
+
+  // トップページ
+  gulp.src(indexFile)
+    .pipe(ejs({ list }))
+    .pipe(rename('index.vue'))
+    .pipe(gulp.dest(dest));
+
+  // 企画画像
+  gulp.src('./booths/images/*')
+    .pipe(gulp.dest('./assets/image/booths/'));
+
+  // 1企画ごとに1ページ作成
+  for (let i = 0; i < list.length; i++) {
+    id = list[i].id;
+
+    gulp.src(templateFile)
+      .pipe(ejs({
+        boothInfo: list[i],
+        prevBoothInfo: i !== 0 ? list[i - 1] : null,
+        nextBoothInfo: i !== list.length - 1 ? list[i + 1] : null,
+      }))
+      .pipe(rename(id + '.vue'))
+      .pipe(gulp.dest(dest));
+  }
+
+  done();
+});
