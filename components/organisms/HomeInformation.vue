89c1c5d7362582184392f57aa1360159f5a6f7ac
Shin-hub <53927466+Shin-hub@users.noreply.github.com>
2020-03-16 12:01:16 +0900

    fix51 (#52)
    Co-authored-by: shin <zsanpu78@gmail.com>

--- components/organisms/HomeInformation.vue
+++ components/organisms/HomeInformation.vue
@@ -14,10 +14,12 @@
               <fa icon="door-open" fixed-width /> 開催日時
             </h2>
             <p class="information__article">
-              2020年4月11日<small class="information__article__small"
+              2020年4月
+              <!-- 11日<small class="information__article__small"
                 >(土曜日)</small
-              ><br />
-              11:00～15:00
+              > -->
+              <br />
+              11:00 ～ 15:00
             </p>
             <h2 class="information__subtitle">
               <fa icon="door-open" fixed-width /> 場所
