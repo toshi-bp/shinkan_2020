4933077956ca86855580be7d3a0262ed38d1a9ea
Toshi <yttoshi0919@gmail.com>
2020-04-08 16:02:11 +0900

    特集記事作れるようになったよ #59
    Co-authored-by: Shin-hub <miura113@icloud.com>

---
+++ pages/articles/1.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <FeatureArticle>
+      <template v-slot:title>
+        金村の会に入ってみたwww
+      </template>
+      <!-- </template> -->
+      <template v-slot:body>
+        <MarkdownPreview :markdownHtml="special"/>
+      </template>
+    </FeatureArticle>
+  </div>
+</template>
+
+<script>
+import makeHead from '~/utils/makeHead';
+import FeatureArticle from '~/components/organisms/FeatureArticle.vue';
+import MarkdownPreview from '~/components/atoms/MarkdownPreview.vue';
+import special from '~/assets/markdown/special.md';
+
+export default {
+  head() {
+    return makeHead(
+      '奏メシ',
+      '理大祭にはいろんな食べ物がいっぱい！',
+    );
+  },
+  components: {
+    FeatureArticle,
+    MarkdownPreview,
+  },
+  computed: {
+    special() {
+      return special;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
